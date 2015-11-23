78208506d8e7931c67c99bd81c378f6babe07866
William Jackson <william@subtlecoolness.com>
2015-11-23 13:25:25 -0600

    Add aiohttp dependency

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='slirck',
     version='0.1',
+    install_requires=['aiohttp'],
     packages=find_packages(),
     entry_points={
         'console_scripts': [
