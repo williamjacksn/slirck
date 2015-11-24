2b08d861c249d99c813dffdfa878499126c3dc75
William Jackson <william@subtlecoolness.com>
2015-11-24 16:30:48 -0600

    Update to version 0.9.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='slirck',
-    version='0.9',
+    version='0.9.1',
     install_requires=['aiohttp'],
     packages=find_packages(),
     entry_points={
